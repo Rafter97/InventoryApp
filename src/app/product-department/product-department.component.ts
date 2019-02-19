2d7498783f0b650c3b2ad98c8390f0d51de94ccd
Mark Rafter <sligo987@gmail.com>
2019-02-19 20:43:51 +0000

    bug Free For Now

--- src/app/product-department/product-department.component.ts
+++ src/app/product-department/product-department.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { Product } from '../product/product.component';
+import { Product } from '../product/products.model';
 
 
 @Component({
--- src/app/product/products.model.ts
+++ src/app/product/products.model.ts
@@ -1,4 +1,4 @@
-export class Products {
+export class Product {
 
     constructor(
         public sku: string,
